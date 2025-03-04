4f95c88f46fc877039406b75b345c13a05c7d8c4
cl0ete <cloete.dupreez@gmail.com>
2025-03-04 11:40:59 +0200

    :art:

--- app/services/issuer/acapy_issuer_v2.py
+++ app/services/issuer/acapy_issuer_v2.py
@@ -75,7 +75,7 @@ class IssuerV2(Issuer):
                 issuer_id=credential.anoncreds_credential_detail.issuer_id,
             )
             cred_filter = V20CredFilter(anoncreds=anon_model)
-            
+
         else:
             raise CloudApiException(
                 f"Unsupported credential type: {credential.type}", status_code=501
