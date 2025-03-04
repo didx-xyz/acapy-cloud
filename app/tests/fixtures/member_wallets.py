03c9be5d16e8e7156d22309d81d97be9cb40f6cf
ff137 <ff137@proton.me>
2025-03-04 20:32:50 +0200

    :test_tube: Test with alice using askar-anoncreds wallet

--- app/tests/fixtures/member_wallets.py
+++ app/tests/fixtures/member_wallets.py
@@ -20,7 +20,7 @@ async def alice_tenant(request) -> AsyncGenerator[CreateTenantResponse, Any]:
 
     async with get_tenant_admin_client() as admin_client:
         if test_mode == TestMode.clean_run:
-            tenant = await create_tenant(admin_client, "alice")
+            tenant = await create_tenant(admin_client, "alice", "askar-anoncreds")
 
             yield tenant
 
