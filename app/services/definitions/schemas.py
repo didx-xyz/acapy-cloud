b7fd1b74645df7a103608c17fa92b10db68220d9
cl0ete <cloete.dupreez@gmail.com>
2025-03-06 10:40:29 +0200

    import until functions

--- app/services/definitions/schemas.py
+++ app/services/definitions/schemas.py
@@ -22,6 +22,7 @@ from app.routes.trust_registry import (
 from app.routes.trust_registry import get_schemas as get_trust_registry_schemas
 from app.services.definitions.schema_publisher import SchemaPublisher
 from app.util.definitions import schema_from_acapy
+from app.util.tenants import get_wallet_id_from_b64encoded_jwt, is_anoncreds_wallet
 from shared.constants import GOVERNANCE_AGENT_URL
 from shared.log_config import get_logger
 
