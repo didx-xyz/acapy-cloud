372b572828128669f983e26fc77c4aaf082261c5
cl0ete <cloete.dupreez@gmail.com>
2025-03-06 11:29:28 +0200

    rename and add old converters

--- app/util/definitions.py
+++ app/util/definitions.py
@@ -1,15 +1,33 @@
 from aries_cloudcontroller import CredentialDefinition as AcaPyCredentialDefinition
-from aries_cloudcontroller import GetSchemaResult, SchemaState
+from aries_cloudcontroller import GetSchemaResult, ModelSchema, SchemaState
 
 from app.models.definitions import CredentialDefinition, CredentialSchema
 
 
-def credential_schema_from_acapy(schema: SchemaState):
+def credential_anon_schema_from_acapy(schema: SchemaState):
     return CredentialSchema(
         id=schema.schema_id,
         name=schema.var_schema.name,
         version=schema.var_schema.version,
         attribute_names=schema.var_schema.attr_names,
+    )
+
+
+def credential_schema_from_acapy(schema: ModelSchema):
+    return CredentialSchema(
+        id=schema.id,
+        name=schema.name,
+        version=schema.version,
+        attribute_names=schema.attr_names,
+    )
+
+
+def anon_schema_from_acapy(schema: GetSchemaResult):
+    return CredentialSchema(
+        id=schema.schema_id,
+        attribute_names=schema.var_schema.attr_names,
+        name=schema.var_schema.name,
+        version=schema.var_schema.version,
     )
 
 
@@ -18,13 +36,4 @@ def credential_definition_from_acapy(credential_definition: AcaPyCredentialDefin
         id=credential_definition.id,
         tag=credential_definition.tag,
         schema_id=credential_definition.schema_id,
-    )
-
-
-def schema_from_acapy(schema: GetSchemaResult):
-    return CredentialSchema(
-        id=schema.schema_id,
-        attribute_names=schema.var_schema.attr_names,
-        name=schema.var_schema.name,
-        version=schema.var_schema.version,
     )
