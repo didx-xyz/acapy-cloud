d314e16de420cf99f636cd69a7af5177ddbce7e9
cl0ete <cloete.dupreez@gmail.com>
2025-03-07 10:10:39 +0200

    get function and call

--- app/routes/revocation.py
+++ app/routes/revocation.py
@@ -370,22 +370,19 @@ async def fix_revocation_registry_entry_state(
     async with client_from_auth(auth) as aries_controller:
         wallet_type = get_wallet_type(aries_controller, bound_logger)
         if wallet_type == "anoncreds":
-            bound_logger.debug("Fixing revocation registry entry state")
-            response = await handle_acapy_call(
-                logger=bound_logger,
-                acapy_call=aries_controller.anoncreds_revocation.update_rev_reg_revoked_state,
-                rev_reg_id=revocation_registry_id,
-                apply_ledger_update=apply_ledger_update,
+            acapy_call = (
+                aries_controller.anoncreds_revocation.update_rev_reg_revoked_state
             )
 
         elif wallet_type == "askar":
-            bound_logger.debug("Fixing revocation registry entry state")
-            response = await handle_acapy_call(
-                logger=bound_logger,
-                acapy_call=aries_controller.revocation.update_rev_reg_revoked_state,
-                rev_reg_id=revocation_registry_id,
-                apply_ledger_update=apply_ledger_update,
-            )
+            acapy_call = aries_controller.revocation.update_rev_reg_revoked_state
+
+        response = await handle_acapy_call(
+            logger=bound_logger,
+            acapy_call=acapy_call,
+            rev_reg_id=revocation_registry_id,
+            apply_ledger_update=apply_ledger_update,
+        )
 
     bound_logger.debug("Successfully fixed revocation registry entry state.")
     return response
