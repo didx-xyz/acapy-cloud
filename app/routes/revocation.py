85ba77ace75232198e5a84ba1645abecfe8b49bb
cl0ete <cloete.dupreez@gmail.com>
2025-03-07 09:55:06 +0200

    TODO

--- app/routes/revocation.py
+++ app/routes/revocation.py
@@ -196,7 +196,8 @@ async def publish_revocations(
         if not result:
             bound_logger.debug("No revocations to publish.")
             return RevokedResponse()
-
+        # TODO The anoncreds response seems to be broken, looks like agent response model not what it's
+        # supposed to be. Need to investigate further.
         endorser_transaction_ids = [txn.transaction_id for txn in result.txn]
         for endorser_transaction_id in endorser_transaction_ids:
             bound_logger.debug(
