91d269bf09771f31b19838d2a8c12d7103197d46
cl0ete <cloete.dupreez@gmail.com>
2025-03-10 11:06:31 +0200

    patch get wallet type

--- app/tests/routes/revocation/test_get_credential_revocation_record.py
+++ app/tests/routes/revocation/test_get_credential_revocation_record.py
@@ -68,7 +68,10 @@ async def test_get_credential_revocation_record_fail_acapy_error(
         "app.routes.revocation.client_from_auth"
     ) as mock_client_from_auth, pytest.raises(
         HTTPException, match=expected_detail
-    ) as exc:
+    ) as exc, patch(
+        "app.services.revocation_registry.get_wallet_type"
+    ) as mock_get_wallet_type:
+        mock_get_wallet_type.return_value = "askar"
         mock_client_from_auth.return_value.__aenter__.return_value = (
             mock_aries_controller
         )
