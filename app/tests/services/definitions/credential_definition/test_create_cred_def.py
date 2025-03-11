144744296e55f1db4cc310b2f44e935dd3bce8bb
ff137 <ff137@proton.me>
2025-03-11 11:42:41 +0200

    :white_check_mark: Add 100% test coverage for CredentialDefinitions service

--- app/tests/services/definitions/credential_definition/test_create_cred_def.py
+++ app/tests/services/definitions/credential_definition/test_create_cred_def.py
@@ -1,19 +1,45 @@
 from unittest.mock import AsyncMock, MagicMock, patch
 
 import pytest
-from aries_cloudcontroller import AcaPyClient, CredentialDefinitionSendRequest
+from aries_cloudcontroller import (
+    AcaPyClient,
+    CredDefResult,
+    CredDefState,
+    CredentialDefinitionSendRequest,
+    CredentialDefinitionSendResult,
+    TxnOrCredentialDefinitionSendResult,
+)
 
 from app.models.definitions import CreateCredentialDefinition
 from app.services.definitions.credential_definitions import create_credential_definition
 
+sample_cred_def_id = "J5Pvam9KqK8ZPQWtvhAxSx:3:CL:8:Epic"
 
+
+@pytest.mark.parametrize("wallet_type", ["askar", "askar-anoncreds"])
 @pytest.mark.anyio
-async def test_create_credential_definition_success():
+async def test_create_credential_definition_success(wallet_type):
     mock_aries_controller = AsyncMock(spec=AcaPyClient)
     mock_publisher = AsyncMock()
-    mock_publisher.publish_credential_definition.return_value = MagicMock(
-        sent=MagicMock(credential_definition_id="test_cred_def_id"), txn=None
-    )
+
+    if wallet_type == "askar":
+        mock_publisher.publish_credential_definition.return_value = (
+            TxnOrCredentialDefinitionSendResult(
+                sent=CredentialDefinitionSendResult(
+                    credential_definition_id=sample_cred_def_id
+                ),
+                txn=None,
+            )
+        )
+    else:  # wallet_type == "askar-anoncreds"
+        mock_publisher.publish_anoncreds_credential_definition.return_value = (
+            CredDefResult(
+                credential_definition_state=CredDefState(
+                    credential_definition_id=sample_cred_def_id
+                ),
+                registration_metadata={"txn": None},
+            )
+        )
 
     create_cred_def_payload = CreateCredentialDefinition(
         schema_id="test_schema_id", tag="test_tag", support_revocation=False
@@ -32,7 +58,7 @@ async def test_create_credential_definition_success():
     ) as mock_handle_model, patch(
         "app.services.definitions.credential_definitions.get_wallet_type"
     ) as mock_get_wallet_type:
-        mock_get_wallet_type.return_value = "askar"
+        mock_get_wallet_type.return_value = wallet_type
 
         mock_handle_model.return_value = CredentialDefinitionSendRequest(
             schema_id="CXQseFxV34pcb8vf32XhEa:2:test_schema:0.3.0",
@@ -44,9 +70,14 @@ async def test_create_credential_definition_success():
             mock_aries_controller, create_cred_def_payload, False
         )
 
-        assert result == "test_cred_def_id"
-        mock_publisher.publish_credential_definition.assert_called_once()
-        mock_handle_model.assert_called_once()
+        assert result == sample_cred_def_id
+
+        if wallet_type == "askar":
+            mock_handle_model.assert_called_once()
+            mock_publisher.publish_credential_definition.assert_called_once()
+        else:  # wallet_type == "askar-anoncreds"
+            assert mock_handle_model.call_count == 3
+            mock_publisher.publish_anoncreds_credential_definition.assert_called_once()
 
 
 @pytest.mark.anyio
@@ -54,7 +85,7 @@ async def test_create_credential_definition_with_revocation():
     mock_aries_controller = AsyncMock(spec=AcaPyClient)
     mock_publisher = AsyncMock()
     mock_publisher.publish_credential_definition.return_value = MagicMock(
-        sent=MagicMock(credential_definition_id="test_cred_def_id"),
+        sent=MagicMock(credential_definition_id=sample_cred_def_id),
         txn=MagicMock(transaction_id="test_txn_id"),
     )
 
@@ -85,11 +116,11 @@ async def test_create_credential_definition_with_revocation():
             mock_aries_controller, create_cred_def_payload, True
         )
 
-        assert result == "test_cred_def_id"
+        assert result == sample_cred_def_id
         mock_publisher.check_endorser_connection.assert_called_once()
         mock_publisher.publish_credential_definition.assert_called_once()
         mock_publisher.wait_for_revocation_registry.assert_called_once_with(
-            credential_definition_id="test_cred_def_id", wallet_type="askar"
+            credential_definition_id=sample_cred_def_id, wallet_type="askar"
         )
 
 
