1783076f155ef0bfa4b986d37d47a0e5026b5603
cl0ete <cloete.dupreez@gmail.com>
2025-03-13 11:04:50 +0200

    ✅ Oob unit tests (#1380)
    * if no body create connection invitation
    
    * test accept oob
    
    * :art:
    
    * test connect to pub did
    
    * test create oob invitation
    
    * remove imports
    
    * remove imports
    
    * update example string

---
+++ app/tests/routes/oob/test_accept_oob_invitation.py
@@ -0,0 +1,45 @@
+from unittest.mock import AsyncMock, patch
+
+import pytest
+from aries_cloudcontroller import OobRecord
+
+from app.models.oob import AcceptOobInvitation
+from app.routes.oob import accept_oob_invitation
+
+test_oob_record = OobRecord(
+    invi_msg_id="some_invitation_id",
+    invitation={},
+    oob_id="some_oob_id",
+    state="await-response",
+)
+
+
+@pytest.mark.anyio
+async def test_accept_oob_invitation_success():
+    mock_aries_controller = AsyncMock()
+    mock_aries_controller.out_of_band.receive_invitation = AsyncMock(
+        return_value=test_oob_record
+    )
+    body = AcceptOobInvitation(
+        alias="test_alias",
+        use_existing_connection=False,
+        invitation={},
+    )
+    with patch("app.routes.oob.client_from_auth") as mock_client_from_auth:
+        mock_client_from_auth.return_value.__aenter__.return_value = (
+            mock_aries_controller
+        )
+
+        response = await accept_oob_invitation(
+            body=body,
+            auth="mocked_auth",
+        )
+
+        assert response == test_oob_record
+
+        mock_aries_controller.out_of_band.receive_invitation.assert_awaited_once_with(
+            auto_accept=True,
+            use_existing_connection=body.use_existing_connection,
+            alias=body.alias,
+            body=body.invitation,
+        )
---
+++ app/tests/routes/oob/test_create_oob_invitation.py
@@ -0,0 +1,65 @@
+from unittest.mock import AsyncMock, patch
+
+import pytest
+from aries_cloudcontroller import (
+    AttachmentDef,
+    InvitationCreateRequest,
+    InvitationRecord,
+)
+
+from app.models.oob import CreateOobInvitation
+from app.routes.oob import create_oob_invitation
+
+test_invitation_record = InvitationRecord(
+    invitation_url="https://example.com?oob=12345",
+    invitation_msg={},
+    invitation_id="some_invitation_id",
+)
+
+test_attachments = [AttachmentDef(id="test_id", type="credential-offer")]
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize(
+    "body",
+    [
+        None,
+        CreateOobInvitation(create_connection=True),
+        CreateOobInvitation(attachments=test_attachments),
+    ],
+)
+async def test_create_oob_invitation_success(body):
+    mock_aries_controller = AsyncMock()
+    mock_aries_controller.out_of_band.create_invitation = AsyncMock(
+        return_value=test_invitation_record
+    )
+    handshake_protocols = [
+        "https://didcomm.org/didexchange/1.0",
+        "https://didcomm.org/connections/1.0",
+    ]
+    if body and body.attachments:
+        handshake_protocols = None
+
+    create_request = InvitationCreateRequest(
+        alias=body.alias if body else None,
+        attachments=body.attachments if body else None,
+        handshake_protocols=handshake_protocols,
+        use_public_did=body.use_public_did if body else None,
+    )
+    with patch("app.routes.oob.client_from_auth") as mock_client_from_auth:
+        mock_client_from_auth.return_value.__aenter__.return_value = (
+            mock_aries_controller
+        )
+
+        response = await create_oob_invitation(
+            body=body,
+            auth="mocked_auth",
+        )
+
+        assert response == test_invitation_record
+
+        mock_aries_controller.out_of_band.create_invitation.assert_awaited_once_with(
+            multi_use=body.multi_use if body else None,
+            body=create_request,
+            auto_accept=True,
+        )
