3e203284edba728d10564624c023fda65906a017
ff137 <ff137@proton.me>
2025-03-19 20:20:19 +0200

    :art: Deduplicate

--- app/tests/e2e/issuer/test_connections_use_public_did.py
+++ app/tests/e2e/issuer/test_connections_use_public_did.py
@@ -10,14 +10,25 @@ CONNECTIONS_BASE_PATH = router.prefix
 
 
 @pytest.mark.anyio
-@pytest.mark.xdist_group(name="issuer_test_group_3")
+@pytest.mark.xdist_group(name="issuer_test_group")
+@pytest.mark.parametrize("issuer_wallet_type", ["indy", "anoncreds"])
 async def test_accept_use_public_did(
+    issuer_wallet_type: str,
     faber_indy_client: RichAsyncClient,  # issuer has public did
+    faber_anoncreds_client: RichAsyncClient,
     meld_co_indy_client: RichAsyncClient,  # also has public did
+    meld_co_anoncreds_client: RichAsyncClient,
 ):
+    if issuer_wallet_type == "indy":
+        issuer_client = faber_indy_client
+        issuer_verifier_client = meld_co_indy_client
+    else:
+        issuer_client = faber_anoncreds_client
+        issuer_verifier_client = meld_co_anoncreds_client
+
     invite_json = CreateInvitation(use_public_did=True).model_dump()
 
-    response = await faber_indy_client.post(
+    response = await issuer_client.post(
         f"{CONNECTIONS_BASE_PATH}/create-invitation", json=invite_json
     )
     assert response.status_code == 200
@@ -33,7 +44,7 @@ async def test_accept_use_public_did(
         invitation=invitation["invitation"],
     ).model_dump()
 
-    accept_response = await meld_co_indy_client.post(
+    accept_response = await issuer_verifier_client.post(
         f"{CONNECTIONS_BASE_PATH}/accept-invitation",
         json=accept_invite_json,
     )
@@ -45,7 +56,7 @@ async def test_accept_use_public_did(
     assert_that(connection_record).has_state("request-sent")
 
     assert await check_webhook_state(
-        client=meld_co_indy_client,
+        client=issuer_verifier_client,
         topic="connections",
         state="completed",
         filter_map={
@@ -55,104 +66,22 @@ async def test_accept_use_public_did(
 
 
 @pytest.mark.anyio
-@pytest.mark.xdist_group(name="issuer_test_group_4")
+@pytest.mark.xdist_group(name="issuer_test_group")
+@pytest.mark.parametrize("issuer_wallet_type", ["indy", "anoncreds"])
 async def test_accept_use_public_did_between_issuer_and_holder(
+    issuer_wallet_type: str,
     faber_indy_client: RichAsyncClient,  # issuer has public did
+    faber_anoncreds_client: RichAsyncClient,
     alice_member_client: RichAsyncClient,  # no public did
 ):
+    if issuer_wallet_type == "indy":
+        issuer_client = faber_indy_client
+    else:
+        issuer_client = faber_anoncreds_client
+
     invite_json = CreateInvitation(use_public_did=True).model_dump()
 
-    response = await faber_indy_client.post(
-        f"{CONNECTIONS_BASE_PATH}/create-invitation", json=invite_json
-    )
-    assert response.status_code == 200
-
-    invitation = response.json()
-    assert_that(invitation["connection_id"]).is_not_empty()
-    assert_that(invitation["invitation"]).is_instance_of(dict).contains(
-        "@id", "@type", "recipientKeys", "serviceEndpoint"
-    )
-    assert_that(invitation["invitation_url"]).matches(r"^https?://")
-
-    accept_invite_json = AcceptInvitation(
-        invitation=invitation["invitation"]
-    ).model_dump()
-
-    accept_response = await alice_member_client.post(
-        f"{CONNECTIONS_BASE_PATH}/accept-invitation",
-        json=accept_invite_json,
-    )
-    connection_record = accept_response.json()
-
-    assert_that(connection_record).contains(
-        "connection_id", "state", "created_at", "updated_at", "invitation_key"
-    )
-    assert_that(connection_record).has_state("request-sent")
-
-    assert await check_webhook_state(
-        client=alice_member_client,
-        topic="connections",
-        state="completed",
-        filter_map={
-            "connection_id": connection_record["connection_id"],
-        },
-    )
-
-
-@pytest.mark.anyio
-@pytest.mark.xdist_group(name="issuer_test_group")
-async def test_accept_use_public_did_anoncreds_wallet(
-    faber_anoncreds_client: RichAsyncClient,  # issuer has public did
-    meld_co_anoncreds_client: RichAsyncClient,  # also has public did
-):
-    invite_json = CreateInvitation(use_public_did=True).model_dump()
-
-    response = await faber_anoncreds_client.post(
-        f"{CONNECTIONS_BASE_PATH}/create-invitation", json=invite_json
-    )
-    assert response.status_code == 200
-
-    invitation = response.json()
-    assert_that(invitation["connection_id"]).is_not_empty()
-    assert_that(invitation["invitation"]).is_instance_of(dict).contains(
-        "@id", "@type", "recipientKeys", "serviceEndpoint"
-    )
-    assert_that(invitation["invitation_url"]).matches(r"^https?://")
-
-    accept_invite_json = AcceptInvitation(
-        invitation=invitation["invitation"],
-    ).model_dump()
-
-    accept_response = await meld_co_anoncreds_client.post(
-        f"{CONNECTIONS_BASE_PATH}/accept-invitation",
-        json=accept_invite_json,
-    )
-    connection_record = accept_response.json()
-
-    assert_that(connection_record).contains(
-        "connection_id", "state", "created_at", "updated_at", "invitation_key"
-    )
-    assert_that(connection_record).has_state("request-sent")
-
-    assert await check_webhook_state(
-        client=meld_co_anoncreds_client,
-        topic="connections",
-        state="completed",
-        filter_map={
-            "connection_id": connection_record["connection_id"],
-        },
-    )
-
-
-@pytest.mark.anyio
-@pytest.mark.xdist_group(name="issuer_test_group")
-async def test_accept_use_public_did_between_anoncreds_issuer_and_holder(
-    faber_anoncreds_client: RichAsyncClient,  # issuer has public did
-    alice_member_client: RichAsyncClient,  # no public did
-):
-    invite_json = CreateInvitation(use_public_did=True).model_dump()
-
-    response = await faber_anoncreds_client.post(
+    response = await issuer_client.post(
         f"{CONNECTIONS_BASE_PATH}/create-invitation", json=invite_json
     )
     assert response.status_code == 200
--- app/tests/e2e/issuer/test_get_records_paginated.py
+++ app/tests/e2e/issuer/test_get_records_paginated.py
@@ -16,164 +16,29 @@ CREDENTIALS_BASE_PATH = router.prefix
     TestMode.regression_run in TestMode.fixture_params,
     reason="Temporarily skip; existing tests on dev don't clean up old records yet",
 )
-@pytest.mark.xdist_group(name="issuer_test_group_4")
+@pytest.mark.xdist_group(name="issuer_test_group")
+@pytest.mark.parametrize("issuer_wallet_type", ["indy", "anoncreds"])
 async def test_get_credential_exchange_records_paginated(
+    issuer_wallet_type: str,
     faber_indy_client: RichAsyncClient,
     alice_member_client: RichAsyncClient,
     indy_credential_definition_id: str,
     faber_indy_and_alice_connection: FaberAliceConnect,
-):
-    num_credentials_to_test = 5
-    test_attributes = {"name": "Alice", "age": "44"}
-
-    faber_cred_ex_ids = []
-    alice_cred_ex_records = []
-    try:
-        # Create multiple credential exchanges
-        for i in range(num_credentials_to_test):
-            test_attributes["speed"] = str(i)
-            credential_v2 = {
-                "connection_id": faber_indy_and_alice_connection.faber_connection_id,
-                "indy_credential_detail": {
-                    "credential_definition_id": indy_credential_definition_id,
-                    "attributes": test_attributes,
-                },
-                "save_exchange_record": True,
-            }
-
-            response = await faber_indy_client.post(
-                CREDENTIALS_BASE_PATH, json=credential_v2
-            )
-
-            credential_exchange_id = response.json()["credential_exchange_id"]
-            faber_cred_ex_ids.append(credential_exchange_id)
-
-        # Test different limits
-        for limit in range(1, num_credentials_to_test + 2):
-            num_tries = 0
-            retry = True
-            while retry and num_tries < 5:  # Handle case where record doesn't exist yet
-                response = await alice_member_client.get(
-                    CREDENTIALS_BASE_PATH,
-                    params={
-                        "state": "offer-received",
-                        "limit": limit,
-                    },
-                )
-                credentials = response.json()
-                expected_num = min(limit, num_credentials_to_test)
-                if len(credentials) != expected_num:
-                    num_tries += 1
-                    await asyncio.sleep(0.2)
-                else:
-                    retry = False
-            assert (
-                not retry
-            ), f"Expected {expected_num} records, got {len(credentials)}: {credentials}"
-
-        # Test ascending order
-        response = await alice_member_client.get(
-            CREDENTIALS_BASE_PATH,
-            params={
-                "state": "offer-received",
-                "limit": num_credentials_to_test,
-                "descending": False,
-            },
-        )
-        credentials_asc = response.json()
-        assert len(credentials_asc) == num_credentials_to_test
-
-        # Verify that the credentials are in ascending order based on created_at
-        assert credentials_asc == sorted(
-            credentials_asc, key=lambda x: x["created_at"], reverse=False
-        )
-
-        # Test descending order
-        response = await alice_member_client.get(
-            CREDENTIALS_BASE_PATH,
-            params={
-                "state": "offer-received",
-                "limit": num_credentials_to_test,
-                "descending": True,
-            },
-        )
-        credentials_desc = response.json()
-        assert len(credentials_desc) == num_credentials_to_test
-
-        # Verify that the credentials are in descending order based on created_at
-        assert credentials_desc == sorted(
-            credentials_desc, key=lambda x: x["created_at"], reverse=True
-        )
-
-        # Compare ascending and descending order results
-        assert credentials_desc == sorted(
-            credentials_asc, key=lambda x: x["created_at"], reverse=True
-        )
-
-        # Test offset greater than number of records
-        response = await alice_member_client.get(
-            CREDENTIALS_BASE_PATH,
-            params={
-                "state": "offer-received",
-                "limit": 1,
-                "offset": num_credentials_to_test,
-            },
-        )
-        credentials = response.json()
-        assert len(credentials) == 0
-
-        # Test fetching unique records with pagination
-        for offset in range(num_credentials_to_test):
-            response = await alice_member_client.get(
-                CREDENTIALS_BASE_PATH,
-                params={
-                    "state": "offer-received",
-                    "limit": 1,
-                    "offset": offset,
-                },
-            )
-
-            credentials = response.json()
-            assert len(credentials) == 1
-
-            record = credentials[0]
-            assert record not in alice_cred_ex_records
-            alice_cred_ex_records.append(record)
-
-        # Test invalid limit and offset values
-        invalid_params = [
-            {"limit": -1},  # must be positive
-            {"offset": -1},  # must be positive
-            {"limit": 0},  # must be greater than 0
-            {"limit": 10001},  # must be less than or equal to max in ACA-Py: 10'000
-        ]
-
-        for params in invalid_params:
-            with pytest.raises(HTTPException) as exc:
-                await faber_indy_client.get(CREDENTIALS_BASE_PATH, params=params)
-            assert exc.value.status_code == 422
-
-    finally:
-        # Clean up created credential exchange records
-        for cred_ex_id in faber_cred_ex_ids:
-            await faber_indy_client.delete(f"{CREDENTIALS_BASE_PATH}/{cred_ex_id}")
-        for alice_record in alice_cred_ex_records:
-            cred_ex_id = alice_record["credential_exchange_id"]
-            await alice_member_client.delete(f"{CREDENTIALS_BASE_PATH}/{cred_ex_id}")
-
-
-@pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason="Temporarily skip; existing tests on dev don't clean up old records yet",
-)
-@pytest.mark.xdist_group(name="issuer_test_group")
-async def test_get_credential_exchange_records_paginated_anoncreds_wallet(
     faber_anoncreds_client: RichAsyncClient,
-    alice_member_client: RichAsyncClient,
     anoncreds_credential_definition_id: str,
     faber_anoncreds_and_alice_connection: FaberAliceConnect,
 ):
+    if issuer_wallet_type == "indy":
+        issuer_client = faber_indy_client
+        connection = faber_indy_and_alice_connection
+        credential_definition_id = indy_credential_definition_id
+    else:
+        issuer_client = faber_anoncreds_client
+        connection = faber_anoncreds_and_alice_connection
+        credential_definition_id = anoncreds_credential_definition_id
+
+    issuer_connection_id = connection.faber_connection_id
+
     num_credentials_to_test = 5
     test_attributes = {"name": "Alice", "age": "44"}
 
@@ -184,16 +49,15 @@ async def test_get_credential_exchange_records_paginated_anoncreds_wallet(
         for i in range(num_credentials_to_test):
             test_attributes["speed"] = str(i)
             credential_v2 = {
-                "type": "anoncreds",
-                "connection_id": faber_anoncreds_and_alice_connection.faber_connection_id,
-                "anoncreds_credential_detail": {
-                    "credential_definition_id": anoncreds_credential_definition_id,
+                "connection_id": issuer_connection_id,
+                "indy_credential_detail": {
+                    "credential_definition_id": credential_definition_id,
                     "attributes": test_attributes,
                 },
                 "save_exchange_record": True,
             }
 
-            response = await faber_anoncreds_client.post(
+            response = await issuer_client.post(
                 CREDENTIALS_BASE_PATH, json=credential_v2
             )
 
@@ -302,13 +166,13 @@ async def test_get_credential_exchange_records_paginated_anoncreds_wallet(
 
         for params in invalid_params:
             with pytest.raises(HTTPException) as exc:
-                await faber_anoncreds_client.get(CREDENTIALS_BASE_PATH, params=params)
+                await issuer_client.get(CREDENTIALS_BASE_PATH, params=params)
             assert exc.value.status_code == 422
 
     finally:
         # Clean up created credential exchange records
         for cred_ex_id in faber_cred_ex_ids:
-            await faber_anoncreds_client.delete(f"{CREDENTIALS_BASE_PATH}/{cred_ex_id}")
+            await issuer_client.delete(f"{CREDENTIALS_BASE_PATH}/{cred_ex_id}")
         for alice_record in alice_cred_ex_records:
             cred_ex_id = alice_record["credential_exchange_id"]
             await alice_member_client.delete(f"{CREDENTIALS_BASE_PATH}/{cred_ex_id}")
