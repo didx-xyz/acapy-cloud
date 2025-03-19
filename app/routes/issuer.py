d95c09498f18d2f08959a30dd273c3aaedd55a5e
cl0ete <cloete.dupreez@gmail.com>
2025-03-19 15:41:05 +0200

    import util function

--- app/routes/issuer.py
+++ app/routes/issuer.py
@@ -10,7 +10,11 @@ from app.models.issuer import CreateOffer, CredentialType, SendCredential
 from app.services.acapy_ledger import schema_id_from_credential_definition_id
 from app.services.issuer.acapy_issuer_v2 import IssuerV2
 from app.services.trust_registry.util.issuer import assert_valid_issuer
-from app.util.did import did_from_credential_definition_id, qualified_did_sov
+from app.util.did import (
+    did_from_credential_definition_id,
+    qualified_did_sov,
+    strip_qualified_did_sov,
+)
 from app.util.pagination import (
     descending_query_parameter,
     limit_query_parameter,
