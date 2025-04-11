6794c9d2507deb27c14cd0cabdafa89cdfb28425
ff137 <ff137@proton.me>
2025-04-11 15:55:16 +0200

    :fire: Remove deprecated connection endpoints

--- app/routes/connections.py
+++ app/routes/connections.py
@@ -1,11 +1,9 @@
 from typing import List, Optional
 
 from aries_cloudcontroller import (
-    CreateInvitationRequest,
     DIDRotateRequestJSON,
     DIDXRejectRequest,
     Hangup,
-    InvitationResult,
     Rotate,
 )
 from fastapi import APIRouter, Depends
@@ -13,7 +11,6 @@ from fastapi import APIRouter, Depends
 from app.dependencies.acapy_clients import client_from_auth
 from app.dependencies.auth import AcaPyAuth, acapy_auth_from_header
 from app.exceptions import handle_acapy_call
-from app.models.connections import AcceptInvitation, CreateInvitation
 from app.util.pagination import (
     descending_query_parameter,
     limit_query_parameter,
@@ -32,119 +29,6 @@ from shared.models.connection_record import (
 logger = get_logger(__name__)
 
 router = APIRouter(prefix="/v1/connections", tags=["connections"])
-
-
-@router.post(
-    "/create-invitation",
-    summary="Create a Connection Invitation",
-    deprecated=True,
-    response_model=InvitationResult,
-)
-async def create_invitation(
-    body: Optional[CreateInvitation] = None,
-    auth: AcaPyAuth = Depends(acapy_auth_from_header),
-) -> InvitationResult:
-    """
-    Create connection invitation
-    ---
-    This endpoint creates an invitation object for establishing a connection with another tenant.
-
-    For every invitation that is created, there will be a corresponding connection record with a state
-    `invitation-sent`. Once this invitation is accepted, the connection record will transition to a state `completed`.
-
-    If `multi_use` is set to true, the invitation can be re-used and accepted by multiple tenants.
-
-    Creating a multi-use invitation will create a connection record with `invitation_mode: "multi"`
-    (instead of `"once"`), and this connection record will remain in a state `invitation-sent`, until it is deleted.
-    Every time a multi-use invitation is accepted, a new connection record will be created with state `completed`.
-
-    The `use_public_did` parameter determines whether to create an invitation using your public DID.
-    This of course requires that you have a public DID in your wallet, which by default is only true for issuers.
-    If `use_public_did` is set to False (default behaviour), then a random DID will be generated for this invite.
-
-    Request Body:
-    ---
-        body: CreateInvitation
-            alias: str (Optional)
-                An alias for the connection invitation, which will appear as the alias in the connection record.
-            multi_use: bool (default: False)
-                Whether the invitation can be used multiple times.
-            use_public_did: bool (default: False)
-                Whether to use a public did for the invitation.
-
-    Returns:
-    ---
-        InvitationResult
-            Contains an invitation object, an invitation url, and a connection id for this invite.
-    """
-    bound_logger = logger.bind(body=body)
-    bound_logger.debug("POST request received: Create invitation")
-    if body is None:
-        body = CreateInvitation()
-
-    async with client_from_auth(auth) as aries_controller:
-        invitation = await handle_acapy_call(
-            logger=bound_logger,
-            acapy_call=aries_controller.connection.create_invitation,
-            alias=body.alias,
-            auto_accept=True,
-            multi_use=body.multi_use,
-            public=body.use_public_did,
-            body=CreateInvitationRequest(),
-        )
-    bound_logger.debug("Successfully created invitation.")
-    return invitation
-
-
-@router.post(
-    "/accept-invitation",
-    summary="Accept a Connection Invitation",
-    deprecated=True,
-    response_model=Connection,
-)
-async def accept_invitation(
-    body: AcceptInvitation,
-    auth: AcaPyAuth = Depends(acapy_auth_from_header),
-) -> Connection:
-    """
-    Accept connection invitation
-    ---
-    Tenants can use this endpoint to accept a connection invitation.
-
-    The invitation object is obtained from an InvitationResult (the response from creating a connection invitation).
-
-    The `invitation_url` in the InvitationResult can also be used to obtain an invitation; there is a base64 encoded
-    string after the "?oob=" parameter in the url, and this can be decoded to obtain the invitation object.
-
-    A webhook event will be emitted for the other party, on topic `connections`.
-    Their record for the connection will now be in state `completed`.
-
-    Request Body:
-    ---
-        body: AcceptInvitation
-            alias: str (Optional)
-                Alias for the connection invitation.
-            invitation: ConnectionInvitation
-                The invitation object obtained from an InvitationResult, or decoded from an invitation_url.
-
-    Returns:
-    ---
-        Connection
-            The record of your new connection
-    """
-    bound_logger = logger.bind(body=body)
-    bound_logger.debug("POST request received: Accept invitation")
-    async with client_from_auth(auth) as aries_controller:
-        connection_record = await handle_acapy_call(
-            logger=bound_logger,
-            acapy_call=aries_controller.connection.receive_invitation,
-            body=body.invitation,
-            auto_accept=True,
-            alias=body.alias,
-        )
-    result = conn_record_to_connection(connection_record)
-    bound_logger.debug("Successfully accepted invitation.")
-    return result
 
 
 @router.get("", summary="Fetch Connection Records", response_model=List[Connection])
@@ -273,9 +157,6 @@ async def delete_connection_by_id(
 
     If the connection uses the didexchange protocol, then we hangup the connection, such that the other party also has
     their record deleted.
-
-    If the connection uses the deprecated connections protocol, then we just delete the record. The other party will
-    still have their record of the connection, but it will become unusable.
 
     Parameters:
     ---
