6f60bfe3037ac55037ba41cd0a13067afa107ec8
ff137 <ff137@proton.me>
2025-04-24 12:38:40 +0200

    :fire: Drop Indy revocation tests

--- app/tests/e2e/test_revocation.py
+++ app/tests/e2e/test_revocation.py
@@ -24,18 +24,6 @@ skip_regression_test_reason = "Skip publish-revocations in regression mode"
     TestMode.regression_run in TestMode.fixture_params,
     reason=skip_regression_test_reason,
 )
-async def test_clear_pending_revokes_indy(
-    faber_indy_client: RichAsyncClient,
-    revoke_alice_indy_creds: List[CredentialExchange],
-):
-    await clear_pending_revokes(faber_indy_client, revoke_alice_indy_creds)
-
-
-@pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason=skip_regression_test_reason,
-)
 async def test_clear_pending_revokes_anoncreds(
     faber_anoncreds_client: RichAsyncClient,
     revoke_alice_anoncreds: List[CredentialExchange],
@@ -110,47 +98,6 @@ async def clear_pending_revokes(
     TestMode.regression_run in TestMode.fixture_params,
     reason=skip_regression_test_reason,
 )
-async def test_clear_pending_revokes_no_map_indy(
-    faber_indy_client: RichAsyncClient,
-    revoke_alice_indy_creds: List[CredentialExchange],
-):
-    # clear_revoke_response = (
-    await faber_indy_client.post(
-        f"{REVOCATION_BASE_PATH}/clear-pending-revocations",
-        json={"revocation_registry_credential_map": {}},
-    )
-    # ).json()["revocation_registry_credential_map"]
-
-    # todo: aca-py now provides response. Make assertions based on response
-
-    for cred in revoke_alice_indy_creds:
-        rev_record = (
-            await faber_indy_client.get(
-                f"{REVOCATION_BASE_PATH}/revocation/record"
-                + "?credential_exchange_id="
-                + cred.credential_exchange_id
-            )
-        ).json()
-
-        assert rev_record["state"] == "issued"
-
-
-@pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason=skip_regression_test_reason,
-)
-async def test_clear_pending_revokes_bad_payload_indy(
-    faber_indy_client: RichAsyncClient,
-):
-    await clear_pending_revokes_bad_payload("indy", faber_indy_client)
-
-
-@pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason=skip_regression_test_reason,
-)
 async def test_clear_pending_revokes_bad_payload_anoncreds(
     faber_anoncreds_client: RichAsyncClient,
 ):
@@ -158,7 +105,7 @@ async def test_clear_pending_revokes_bad_payload_anoncreds(
 
 
 async def clear_pending_revokes_bad_payload(
-    credential_type: Literal["indy", "anoncreds"],
+    credential_type: Literal["anoncreds"],
     faber_client: RichAsyncClient,
 ):
     with pytest.raises(HTTPException) as exc:
@@ -177,33 +124,6 @@ async def clear_pending_revokes_bad_payload(
 
     assert exc.value.status_code == 422
 
-    if credential_type == "indy":  # Not supported for anoncreds
-        with pytest.raises(HTTPException) as exc:
-            await faber_client.post(
-                f"{REVOCATION_BASE_PATH}/clear-pending-revocations",
-                json={
-                    "revocation_registry_credential_map": {
-                        "WgWxqztrNooG92RXvxSTWv:4:WgWxqztrNooG92RXvxSTWv:3:CL:20:tag:CL_ACCUM:0": []
-                    }
-                },
-            )
-
-        assert exc.value.status_code == 404
-
-
-@pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason=skip_regression_test_reason,
-)
-async def test_publish_all_revocations_for_rev_reg_id_indy(
-    faber_indy_client: RichAsyncClient,
-    revoke_alice_indy_creds: List[CredentialExchange],
-):
-    await publish_all_revocations_for_rev_reg_id(
-        "indy", faber_indy_client, revoke_alice_indy_creds
-    )
-
 
 @pytest.mark.anyio
 @pytest.mark.skipif(
@@ -220,7 +140,7 @@ async def test_publish_all_revocations_for_rev_reg_id_anoncreds(
 
 
 async def publish_all_revocations_for_rev_reg_id(
-    credential_type: Literal["indy", "anoncreds"],
+    credential_type: Literal["anoncreds"],
     faber_client: RichAsyncClient,
     revoke_alice_creds: List[CredentialExchange],
 ):
@@ -245,18 +165,6 @@ async def publish_all_revocations_for_rev_reg_id(
         await asyncio.sleep(10)
 
     await check_revocation_status(faber_client, revoke_alice_creds, "revoked")
-
-
-@pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason=skip_regression_test_reason,
-)
-async def test_publish_all_revocations_no_payload_indy(
-    faber_indy_client: RichAsyncClient,
-    revoke_alice_indy_creds: List[CredentialExchange],
-):
-    await publish_all_revocations_no_payload(faber_indy_client, revoke_alice_indy_creds)
 
 
 @pytest.mark.anyio
@@ -300,18 +208,6 @@ async def check_revocation_status(
         ).json()
 
         assert rev_record["state"] == expected_state
-
-
-@pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason=skip_regression_test_reason,
-)
-async def test_publish_one_revocation_indy(
-    faber_indy_client: RichAsyncClient,
-    revoke_alice_indy_creds: List[CredentialExchange],
-):
-    await publish_one_revocation(faber_indy_client, revoke_alice_indy_creds)
 
 
 @pytest.mark.anyio
@@ -375,17 +271,6 @@ async def publish_one_revocation(
     TestMode.regression_run in TestMode.fixture_params,
     reason=skip_regression_test_reason,
 )
-async def test_publish_revocations_bad_payload_indy(
-    faber_indy_client: RichAsyncClient,
-):
-    await publish_revocations_bad_payload(faber_indy_client)
-
-
-@pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason=skip_regression_test_reason,
-)
 async def test_publish_revocations_bad_payload_anoncreds(
     faber_anoncreds_client: RichAsyncClient,
 ):
@@ -429,18 +314,6 @@ async def publish_revocations_bad_payload(
     TestMode.regression_run in TestMode.fixture_params,
     reason=skip_regression_test_reason,
 )
-async def test_get_pending_revocations_indy(
-    faber_indy_client: RichAsyncClient,
-    revoke_alice_indy_creds: List[CredentialExchange],
-):
-    await get_pending_revocations("indy", faber_indy_client, revoke_alice_indy_creds)
-
-
-@pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason=skip_regression_test_reason,
-)
 async def test_get_pending_revocations_anoncreds(
     faber_anoncreds_client: RichAsyncClient,
     revoke_alice_anoncreds: List[CredentialExchange],
@@ -451,7 +324,7 @@ async def test_get_pending_revocations_anoncreds(
 
 
 async def get_pending_revocations(
-    credential_type: Literal["indy", "anoncreds"],
+    credential_type: Literal["anoncreds"],
     faber_client: RichAsyncClient,
     revoke_alice_creds: List[CredentialExchange],
 ):
@@ -474,31 +347,6 @@ async def get_pending_revocations(
         len(pending_revocations) >= 3
     )  # we expect at least 3 cred_rev_ids can be more if whole module is run
 
-    if credential_type == "indy":  # Not supported for anoncreds
-        await faber_client.post(
-            f"{REVOCATION_BASE_PATH}/clear-pending-revocations",
-            json={"revocation_registry_credential_map": {}},
-        )
-
-        pending_revocations = (
-            await faber_client.get(
-                f"{REVOCATION_BASE_PATH}/get-pending-revocations/{rev_reg_id}"
-            )
-        ).json()["pending_cred_rev_ids"]
-
-        assert pending_revocations == []
-
-
-@pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason=skip_regression_test_reason,
-)
-async def test_get_pending_revocations_bad_payload_indy(
-    faber_indy_client: RichAsyncClient,
-):
-    await get_pending_revocations_bad_payload("indy", faber_indy_client)
-
 
 @pytest.mark.anyio
 @pytest.mark.skipif(
@@ -508,41 +356,13 @@ async def test_get_pending_revocations_bad_payload_indy(
 async def test_get_pending_revocations_bad_payload_anoncreds(
     faber_anoncreds_client: RichAsyncClient,
 ):
-    await get_pending_revocations_bad_payload("anoncreds", faber_anoncreds_client)
-
-
-async def get_pending_revocations_bad_payload(
-    credential_type: Literal["indy", "anoncreds"],
-    faber_client: RichAsyncClient,
-):
     with pytest.raises(HTTPException) as exc:
-        await faber_client.get(f"{REVOCATION_BASE_PATH}/get-pending-revocations/bad")
+        await faber_anoncreds_client.get(
+            f"{REVOCATION_BASE_PATH}/get-pending-revocations/bad"
+        )
 
-    if credential_type == "indy":
-        assert exc.value.status_code == 422
-    else:  # TODO: AnonCreds endpoint doesn't validate rev_reg_id
-        assert exc.value.status_code == 404
-
-
-@pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason=skip_regression_test_reason,
-)
-@pytest.mark.parametrize(
-    "rev_reg_id, status_code",
-    [
-        (
-            "Ddhz428iyF5h96uLUgiuFa:4:Ddhz428iyF5h96uLUgiuFa:3:CL:8:Epic:CL_ACCUM:2e292c76-bc43-496c-a65a-297fc49c21c6",
-            404,
-        ),
-        ("bad_format", 422),
-    ],
-)
-async def test_fix_rev_reg_bad_id_indy(
-    faber_indy_client: RichAsyncClient, rev_reg_id: str, status_code: int
-):
-    await fix_rev_reg_bad_id(faber_indy_client, rev_reg_id, status_code)
+    # TODO: AnonCreds endpoint doesn't validate rev_reg_id
+    assert exc.value.status_code == 404
 
 
 @pytest.mark.anyio
