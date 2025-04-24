0bed0de47489abdace9e00661f291b3d7116078e
ff137 <ff137@proton.me>
2025-04-24 12:38:53 +0200

    :fire: Drop Indy definitions tests

--- app/tests/e2e/test_definitions.py
+++ app/tests/e2e/test_definitions.py
@@ -2,15 +2,12 @@ import pytest
 from aries_cloudcontroller import AcaPyClient
 
 from app.dependencies.auth import (
-    AcaPyAuthVerified,
     acapy_auth_from_header,
     acapy_auth_verified,
 )
-from app.models.definitions import SchemaType
 from app.routes import definitions
 from app.routes.definitions import (
     CreateCredentialDefinition,
-    CreateSchema,
     CredentialSchema,
 )
 from app.routes.definitions import router as definitions_router
@@ -21,32 +18,6 @@ from app.util.string import random_string
 from shared import RichAsyncClient
 
 DEFINITIONS_BASE_PATH = definitions_router.prefix
-
-
-@pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason="Don't create new schemas in regression mode",
-)
-async def test_create_schema(
-    governance_public_did: str, mock_governance_auth: AcaPyAuthVerified
-):
-    send = CreateSchema(
-        schema_type=SchemaType.INDY,
-        name=random_string(15),
-        version="0.1",
-        attribute_names=["average"],
-    )
-
-    result = (await definitions.create_schema(send, mock_governance_auth)).model_dump()
-
-    # Assert schemas has been registered in the trust registry
-    assert await registry_has_schema(result["id"])
-    expected_schema = f"{governance_public_did}:2:{send.name}:{send.version}"
-    assert result["id"] == expected_schema
-    assert result["name"] == send.name
-    assert result["version"] == send.version
-    assert set(result["attribute_names"]) == set(send.attribute_names)
 
 
 @pytest.mark.anyio
@@ -65,64 +36,11 @@ async def test_create_anoncreds_schema(
 
 
 @pytest.mark.anyio
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason="Don't create new schemas in regression mode",
-)
-@pytest.mark.xdist_group(name="issuer_test_group")
-async def test_get_schema(
-    governance_public_did: str,
-    mock_governance_auth: AcaPyAuthVerified,
-    faber_indy_client: RichAsyncClient,
-    faber_anoncreds_client: RichAsyncClient,
-):
-    # given
-    schema = CreateSchema(
-        name=random_string(15), version="0.1", attribute_names=["average"]
-    )
-    schema_name = schema.name
-    schema_version = schema.version
-    schema_attributes = schema.attribute_names
-
-    def assert_schema_response(schema_response: CredentialSchema):
-        # Helper method to assert schema response has expected values
-        assert schema_response.id == schema_id
-        assert schema_response.name == schema_name
-        assert schema_response.version == schema_version
-        assert set(schema_response.attribute_names) == set(schema_attributes)
-
-    create_result = await definitions.create_schema(schema, mock_governance_auth)
-
-    schema_id = create_result.id
-
-    expected_schema_id = f"{governance_public_did}:2:{schema.name}:{schema.version}"
-    assert schema_id == expected_schema_id
-
-    # Assert schema is on the trust registry
-    assert await registry_has_schema(schema_id)
-
-    # The schema can be fetched by governance
-    result = await definitions.get_schema(schema_id, mock_governance_auth)
-    assert_schema_response(result)
-
-    # The schema can be fetched by Indy issuers
-    auth = acapy_auth_from_header(faber_indy_client.headers["x-api-key"])
-    result = await definitions.get_schema(schema_id, auth)
-    assert_schema_response(result)
-
-    # The schema can be fetched by AnonCreds issuers
-    auth = acapy_auth_from_header(faber_anoncreds_client.headers["x-api-key"])
-    result = await definitions.get_schema(schema_id, auth)
-    assert_schema_response(result)
-
-
-@pytest.mark.anyio
 @pytest.mark.xdist_group(name="issuer_test_group")
 async def test_get_anoncreds_schema(
     anoncreds_schema_definition: CredentialSchema,
     faber_anoncreds_client: RichAsyncClient,
     meld_co_anoncreds_client: RichAsyncClient,
-    faber_indy_client: RichAsyncClient,
 ):
     schema_id = anoncreds_schema_definition.id
     schema_name = anoncreds_schema_definition.name
@@ -153,86 +71,7 @@ async def test_get_anoncreds_schema(
     schema_response = schema_response.json()
     assert_schema_response(schema_response)
 
-    # Indy issuers can fetch AnonCreds schemas as well
-    schema_response = await faber_indy_client.get(
-        f"{DEFINITIONS_BASE_PATH}/schemas/{schema_id}"
-    )
-    schema_response = schema_response.json()
-    assert_schema_response(schema_response)
-
-
-@pytest.mark.anyio
-@pytest.mark.parametrize("support_revocation", [False, True])
-@pytest.mark.skipif(
-    TestMode.regression_run in TestMode.fixture_params,
-    reason=(
-        "Mystery -- this test passes continuously if it's the only test, "
-        "but it fails when run a 2nd time in regression mode"
-    ),
-)
-@pytest.mark.xdist_group(name="issuer_test_group")
-async def test_create_credential_definition(
-    indy_schema_definition: CredentialSchema,
-    faber_indy_acapy_client: AcaPyClient,
-    faber_indy_client: RichAsyncClient,
-    support_revocation: bool,
-):
-    schema_id = indy_schema_definition.id
-    tag = random_string(5)
-    credential_definition = CreateCredentialDefinition(
-        schema_id=schema_id,
-        tag=tag,
-        support_revocation=support_revocation,
-    )
-
-    auth = acapy_auth_verified(
-        acapy_auth_from_header(faber_indy_client.headers["x-api-key"])
-    )
-
-    result = (
-        await definitions.create_credential_definition(
-            credential_definition=credential_definition, auth=auth
-        )
-    ).model_dump()
-
-    faber_public_did = await get_public_did(faber_indy_acapy_client)
-    schema = await faber_indy_acapy_client.schema.get_schema(schema_id=schema_id)
-
-    assert (
-        result["id"] == f"{faber_public_did.did}:3:CL:{schema.var_schema.seq_no}:{tag}"
-    )
-    assert result["tag"] == tag
-    assert result["schema_id"] == schema_id
-
-    get_cred_def_result = (
-        await definitions.get_credential_definition_by_id(result["id"], auth)
-    ).model_dump()
-
-    assert get_cred_def_result["tag"] == tag
-    assert get_cred_def_result["schema_id"] == schema_id
-
-    if support_revocation:
-        cred_def_id = result["id"]
-        # Assert that revocation registry was created
-        rev_reg_result = (
-            await faber_indy_acapy_client.revocation.get_active_registry_for_cred_def(
-                cred_def_id=cred_def_id
-            )
-        )
-        issuer_rev_reg_record = rev_reg_result.result
-        assert issuer_rev_reg_record
-        assert cred_def_id == issuer_rev_reg_record.cred_def_id
-        assert issuer_rev_reg_record.issuer_did == faber_public_did.did
-
-        revocation_registries = (
-            await faber_indy_acapy_client.revocation.get_created_registries(
-                cred_def_id=cred_def_id
-            )
-        ).rev_reg_ids
-
-        # There should be two revocation registries, assert at least one exists
-        # one being used to issue credentials against and once full with to the next one
-        assert len(revocation_registries) >= 1
+    # Note: Indy issuers can fetch AnonCreds schemas as well
 
 
 @pytest.mark.anyio
