79e6e6ff47bcf8f36bb17b55263c39c5dc97b776
Mourits de Beer <31511766+ff137@users.noreply.github.com>
2025-05-02 11:34:21 +0200

    :bookmark: 5.0.0rc1 (#1504)
    * :bookmark: 5.0.0rc1
    
    * :memo: Update openapi specs

--- shared/constants.py
+++ shared/constants.py
@@ -6,7 +6,7 @@ adminApiKey = "adminApiKey"
 
 # pylint: disable=invalid-name
 
-PROJECT_VERSION = os.getenv("PROJECT_VERSION", "5.0.0rc0")
+PROJECT_VERSION = os.getenv("PROJECT_VERSION", "5.0.0rc1")
 
 # the ACAPY_LABEL field with which the governance agent is initialised
 GOVERNANCE_LABEL = os.getenv("GOVERNANCE_ACAPY_LABEL", "Governance")
