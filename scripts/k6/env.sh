9752302801abfb1eb673e94061b9bdb0ad77a00a
wdbasson <wlmbasson@gmail.com>
2025-05-13 16:48:35 +0200

    Decrease default number of total iterations to 4

--- scripts/k6/env.sh
+++ scripts/k6/env.sh
@@ -7,11 +7,11 @@ fi
 export K6_STATSD_ENABLE_TAGS=true
 export K6_STATSD_PUSH_INTERVAL=5
 export SKIP_DELETE_ISSUERS=true
-export VUS=5
+export VUS=2
 export ITERATIONS=2
 export ISSUER_PREFIX=k6_issuer
 export HOLDER_PREFIX=k6_holder
 export SCHEMA_PREFIX=k6_schema
-export NUM_ISSUERS=2
+export NUM_ISSUERS=1
 export SCHEMA_NAME=k6_schema
 export SCHEMA_VERSION=1.0
