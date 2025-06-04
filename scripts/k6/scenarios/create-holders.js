14caa95c8a502a788644d6052239dcfab9546779
wdbasson <wlmbasson@gmail.com>
2025-06-04 15:46:42 +0200

    Remove old code and add custom logging to create-holders.js

--- scripts/k6/scenarios/create-holders.js
+++ scripts/k6/scenarios/create-holders.js
@@ -7,6 +7,7 @@ import { Counter, Trend } from "k6/metrics";
 import file from "k6/x/file";
 import { getAuthHeaders } from '../libs/auth.js';
 import { createTenant } from "../libs/functions.js";
+import { log } from "../libs/k6Functions.js";
 
 const vus = Number(__ENV.VUS || 1);
 const iterations = Number(__ENV.ITERATIONS || 1);
@@ -25,17 +26,12 @@ export const options = {
       maxDuration: "24h",
     },
   },
-  setupTimeout: "300s", // Increase the setup timeout to 120 seconds
-  teardownTimeout: "120s", // Increase the teardown timeout to 120 seconds
+  setupTimeout: "300s",
+  teardownTimeout: "120s",
   maxRedirects: 4,
   thresholds: {
     // https://community.grafana.com/t/ignore-http-calls-made-in-setup-or-teardown-in-results/97260/2
-    // "http_req_duration{scenario:default}": ["max>=0"],
-    // "http_reqs{scenario:default}": ["count >= 0"],
-    // "iteration_duration{scenario:default}": ["max>=0"],
     checks: ["rate==1"],
-    // 'specific_function_reqs{my_custom_tag:specific_function}': ['count>=0'],
-    // 'specific_function_reqs{scenario:default}': ['count>=0'],
     'test_function_reqs{my_custom_tag:specific_function}': ['count>=0'],
   },
   tags: {
@@ -45,9 +41,7 @@ export const options = {
   },
 };
 
-// const specificFunctionReqs = new Counter('specific_function_reqs');
 const testFunctionReqs = new Counter("test_function_reqs");
-// const mainIterationDuration = new Trend("main_iteration_duration");
 
 // Seed data: Generating a list of options.iterations unique wallet names
 const wallets = new SharedArray("wallets", () => {
@@ -99,8 +93,6 @@ export default function (data) {
     createTenantResponse.body
   );
 
-  // specificFunctionReqs.add(1, { my_custom_tag: 'specific_function' });
-
   const holderData = JSON.stringify({
     wallet_label: wallet.wallet_label,
     wallet_name: wallet.wallet_name,
@@ -109,10 +101,7 @@ export default function (data) {
   });
   file.appendString(filepath, `${holderData}\n`);
 
-  const end = Date.now();
-  const duration = end - start;
-  // console.log(`Duration for iteration ${__ITER}: ${duration} ms`);
-  // mainIterationDuration.add(duration);
+  log('debug', `Sleeping for ${sleepDuration}ms after creating holder ${wallet.wallet_name}`);
   sleep(sleepDuration);
   testFunctionReqs.add(1);
 }
