4c526c8110393c53d68f6d95327772039b5e3f34
ff137 <ff137@proton.me>
2025-06-05 16:01:53 +0300

    :art: Remove "unused-import" ignore for conftest files

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,5 +1,3 @@
-# flake8: noqa
-# pylint: disable=unused-import
 import pytest
 
 from app.tests.fixtures.dids import register_issuer_key_ed25519
@@ -46,7 +44,43 @@ from shared.util.mock_agent_controller import (
     mock_tenant_auth_verified,
 )
 
-# Unused imports make pytest fixtures visible to tests within this module
+__all__ = [
+    "register_issuer_key_ed25519",
+    "acme_acapy_client",
+    "alice_acapy_client",
+    "bob_acapy_client",
+    "faber_anoncreds_acapy_client",
+    "governance_acapy_client",
+    "meld_co_anoncreds_acapy_client",
+    "tenant_admin_acapy_client",
+    "acme_client",
+    "alice_member_client",
+    "bob_member_client",
+    "faber_anoncreds_client",
+    "governance_client",
+    "meld_co_anoncreds_client",
+    "mock_async_client",
+    "tenant_admin_client",
+    "trust_registry_client",
+    "acme_and_alice_connection",
+    "bob_and_alice_connection",
+    "faber_anoncreds_and_alice_connection",
+    "meld_co_anoncreds_and_alice_connection",
+    "test_mode",
+    "acme_verifier",
+    "alice_tenant",
+    "bob_tenant",
+    "faber_anoncreds_issuer",
+    "meld_co_anoncreds_issuer_verifier",
+    "mock_admin_auth",
+    "mock_agent_controller",
+    "mock_context_managed_controller",
+    "mock_governance_auth",
+    "mock_tenant_auth",
+    "mock_tenant_auth_verified",
+]
+
+# Imports make pytest fixtures visible to tests within this module
 
 # In pytest, conftest.py is a special file used to share fixtures, hooks, and other configurations
 # among multiple test files. It's automatically discovered by pytest when tests are run, and the
