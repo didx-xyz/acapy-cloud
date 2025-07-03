35da6dad889bb129e63c34f8749723cd5e5e3bcb
wdbasson <wlmbasson@gmail.com>
2025-07-03 16:23:10 +0200

    Dedicated execution strategies with clear intent: parallel, serial or concurrent

--- scripts/k6/env-deprecated.sh
+++ scripts/k6/env-deprecated.sh
@@ -12,3 +12,14 @@ else
     export K6_STATSD_ENABLE_TAGS=false
     unset K6_STATSD_ADDR
 fi
+export SKIP_DELETE_ISSUERS=true
+export VUS=4
+export ITERATIONS=1
+export ISSUER_PREFIX=k6_issuer
+export HOLDER_PREFIX=k6_holder
+export SCHEMA_PREFIX=k6_schema
+export NUM_ISSUERS=1
+export SCHEMA_NAME=${SCHEMA_NAME:-k6_schema}
+export SCHEMA_VERSION=${SCHEMA_VERSION:-1.0}
+export OOB_INVITATION=true # temporary until connect via pulbic DID on Cheqd is implemented
+export USE_AUTO_PUBLISH=true
